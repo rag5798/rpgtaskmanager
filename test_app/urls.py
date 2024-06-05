0629be61163c9d2069a572b04cf6f7d884014718
Sara Leal Silverio <saralealsilverio@gmail.com>
2024-06-05 10:25:41 -0600

    url fix

--- test_app/urls.py
+++ test_app/urls.py
@@ -3,6 +3,6 @@ from test_app import views
 
 urlpatterns = [
     path("" , views.home),
-    path("test_app/<name>", views.hello_there, name="hello_there"),
+    path("<name>", views.hello_there, name="hello_there"),
     path("task", views.task)
 ]
