2d674f1fc14b5f74ae5ac58ba6b9bd7899c0495d
rag5798 <bgoettman@yahoo.com>
2024-06-07 09:47:09 -0600

    fresh project with proper name and settings

--- sidequest_server/urls.py
+++ sidequest_server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for test_project project.
+URL configuration for sidequest_server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
-    path("", include("test_app.urls")),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
 ]
