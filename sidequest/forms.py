a20eb75524d1dea8cfcc77629b95b482329277ab
rag5798 <bgoettman@yahoo.com>
2024-06-19 10:49:53 -0600

    added new form for tasks

--- sidequest/forms.py
+++ sidequest/forms.py
@@ -4,10 +4,16 @@ from django import forms
 
 
 from django import forms
-from .models import UserData
+from .models import UserData, Task
 
 class UserDataForm(forms.ModelForm):
     class Meta:
         model = UserData
         fields = ['username', 'password']  # Specify the fields you want in the form
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['user_id','task_name','task_description','completed','task_completion_counter']
+
 
